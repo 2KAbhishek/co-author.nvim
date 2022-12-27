def887ea71a797d153acf2eaf37e02a2b680d10e
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-12-27 18:18:33 +0530

    cut: do not set keybindings by default

--- plugin/co-author.lua
+++ plugin/co-author.lua
@@ -6,5 +6,3 @@ vim.g.loaded_co_author = true
 
 vim.cmd('command! GitCoAuthors lua require("co-author").list()')
 
-local opts = { noremap = true, silent = true }
-vim.api.nvim_set_keymap('n', '<leader>gA', ':GitCoAuthors', opts)
