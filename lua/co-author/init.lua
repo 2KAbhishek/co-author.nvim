81b81a3167d770a0e279ab10a1f3a74b8ea0122b
Sumanth Yeodti <sumanth.yedoti@gmail.com>
2023-10-08 18:47:09 +0530

    Fix: error on escape and stylua.toml filename

--- lua/co-author/init.lua
+++ lua/co-author/init.lua
@@ -20,16 +20,18 @@ co_author.list = function()
         items[#items + 1] = author
     end
     if #co_authors > 0 then
-        vim.ui.select(items, { prompt = 'Select Co-Authors' },
-            function(item, _)
-                local string = "Co-authored-by: " .. item
-                local cursor_position = vim.api.nvim_win_get_cursor(0)
-                local line = cursor_position[1]
-                local column = cursor_position[2]
+        vim.ui.select(items, { prompt = 'Select Co-Authors' }, function(item, _)
+            if not item then
+                return
+            end
+            local string = 'Co-authored-by: ' .. item
+            local cursor_position = vim.api.nvim_win_get_cursor(0)
+            local line = cursor_position[1]
+            local column = cursor_position[2]
 
-                vim.api.nvim_buf_set_lines(0, line, line, true, { string })
-                vim.api.nvim_win_set_cursor(0, { line, column + #string })
-            end)
+            vim.api.nvim_buf_set_lines(0, line, line, true, { string })
+            vim.api.nvim_win_set_cursor(0, { line, column + #string })
+        end)
     end
 end
 
